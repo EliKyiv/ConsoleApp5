cc6931be7bcda67b483b2b11483146cb11d45693
INFOPULSE\Illia.Linkov <illia.linkov@infopulse.com>
2023-03-20 19:56:43 +0200

    Comment

--- Program.cs
+++ Program.cs
@@ -22,6 +22,15 @@ static void Main(string[] args) {
         John.Courses = Topics;
         John.Print();
 
+    Course QA = new Course();
+        QA.Name = "QA";
+        QA.Teacher = "Christina";
+        QA.Duration = 30;
+        QA.StudentsQty = 20;
+        QA.Print();
+
+        QA.StudentsQty ++;
+        QA.Print();
     }
 
 }
@@ -38,6 +47,48 @@ public class Student {
         Console.WriteLine(City);
         Console.WriteLine(Age);
         Console.WriteLine($"{string.Join(", ", Courses)}");
-
     }
+    public Student() {
+        Age = 0;
+        Name = string.Empty;
+        Lastname = string.Empty;
+        City = string.Empty;
+    }
+
+    public Student(string name, string lastname, string city, int age) {
+        Name = name;
+        Lastname = lastname;
+        City = city;
+        Age = age;
+    }
+
+}
+
+public class Course { 
+    public string Name { get; set; }
+    public string Teacher { get; set; }
+    public int Duration { get; set; }
+    public int StudentsQty { get; set; }
+
+    public void Print () {
+        Console.WriteLine(Name);
+        Console.WriteLine(Teacher);
+        Console.WriteLine(Duration);
+        Console.WriteLine(StudentsQty);
+    }
+
+    public Course() { 
+        Name = string.Empty;
+        Teacher = string.Empty;
+        Duration = 0;
+        StudentsQty = 0;
+    }
+
+    public Course(string name, string teacher, int duration, int studentsQty) {
+        Name = name;
+        Teacher = teacher;
+        Duration = duration;
+        StudentsQty = studentsQty;
+    }
+
 }
