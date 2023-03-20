c222916dbfda5f21f0cde48b67d4325deb7a0694
INFOPULSE\Illia.Linkov <illia.linkov@infopulse.com>
2023-03-20 19:02:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program { 
+static void Main(string[] args) { 
+    Student John = new Student();
+        John.Name = "Johnny";
+        John.Lastname = "Carter";
+        John.Age = 30;
+        John.City = "Kyiv";
+
+        string[] Topics = { "C#", "SQL", "QA" };
+        John.Courses = Topics;
+
+        John.Print();
+
+        Array.Resize(ref Topics, Topics.Length + 1);
+        Topics[Topics.Length - 1] = "Java";
+        John.Courses = Topics;
+        John.Print();
+    }
+
+}
+
+public class Student {
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public string City { get; set; }
+    public string[] Courses;
+    public void Print() {
+        Console.WriteLine(Name);
+        Console.WriteLine(Lastname);
+        Console.WriteLine(City);
+        Console.WriteLine(Age);
+        Console.WriteLine($"{string.Join(", ", Courses)}");
+
+    }
+}
